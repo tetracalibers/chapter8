e5960e702461c3458886c5bcfe5fead02298c7ac
tetracalibers <tomix05may@gmail.com>
2022-03-18 05:44:26 +0900

    同様にEntryモデルとマイグレーションファイルを作成

---
+++ app/DataAccess/Eloquent/Entry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DataAccess\Eloquent;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    use HasFactory;
+}
